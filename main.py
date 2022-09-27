54002ab2f3740a46bae615fc7b8a3d967bd82df2
sirPinwheel <pinwheelgm+git@gmail.com>
2022-09-27 08:32:11 +0200

    added logging

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
+#!/usr/bin/env python3
+
 import os
 import sys
+import logging
+import logging.handlers
 import discord
 
-PROMPT = os.getenv('PROMPT')
+# Getting environmental variables
+PROMPT = os.getenv('PROMPT') # defaults to '!'
 TOKEN = os.getenv('TOKEN')
+LOG_LEVEL = os.getenv('LOG_LEVEL')
 
 if PROMPT is None:
     PROMPT = '!'
 
 if TOKEN is None:
     sys.exit('No token provided in environmental variables')
+
+if LOG_LEVEL == 'DEBUG':
+    LOG_LEVEL = logging.DEBUG
+else:
+    LOG_LEVEL = logging.INFO
+
+# Setting up logging
+info_format = '[{asctime}] [{levelname:<8}] {name}: {message}'
+date_format = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter(info_format, date_format, style='{')
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='log/debug.log',
+    encoding='utf-8',
+    maxBytes=32*1024*1024, # 32 mib max file size
+    backupCount=1
+)
+
+handler.setFormatter(formatter)
+
+logger = logging.getLogger('discord')
+logger.setLevel(LOG_LEVEL)
+logger.addHandler(handler)
+
 
 def main():
     intents = discord.Intents.default()
@@ -29,7 +59,7 @@ def main():
         if message.content.startswith(PROMPT):
             await message.channel.send('Hello!')
 
-    client.run(TOKEN)
+    client.run(TOKEN, log_handler=None)
 
 if __name__ == "__main__":
     main()
